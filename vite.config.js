1512f3591f9d841e54ac6d7d6265a328fc19cea6
eternitypain <eternaldespair7474@gmail.com>
2024-08-07 14:49:32 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     },
     publicDir: './src/assets',
     build: {
-        outDir: './dist',
+        outDir: '../dist',
         emptyOutDir: true,
         rollupOptions: {
             input: {
